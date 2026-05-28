b95e2ed001e92642a58b0db8252806c86b7cc25e
DarshanPawar07 <darshandpawar234@gmail.com>
2026-05-29 04:41:51 +0530

    fix queueEvents syntax

--- src/queues/queueEvents.ts
+++ src/queues/queueEvents.ts
@@ -1,11 +1,11 @@
 import { QueueEvents } from "bullmq";
 
-import { redisConfig } from "../config/redis";
-
 export const generationQueueEvents =
   new QueueEvents(
     "question-generation-queue",
-    connection: {
-  url: process.env.REDIS_URL,
-}
+    {
+      connection: {
+        url: process.env.REDIS_URL,
+      },
+    }
   );
